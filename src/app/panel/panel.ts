e6d57c2acec05c7a4d684a57cc81d37c13cd5781
Gonçalo Moreira <goncalomoreira01@gmail.com>
2026-02-19 18:58:13 +0000

    [TASK] Changes in the panel
    - now user have "minhas marcações" tab in order to the user see the previous / next schedules;
    - review tab in order to user give feedback

--- src/app/panel/panel.ts
+++ src/app/panel/panel.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -6,11 +6,16 @@ import {provideNativeDateAdapter} from '@angular/material/core';
 import { MatTab } from '@angular/material/tabs';
 import { MatTabGroup } from '@angular/material/tabs';
 import { MatDivider } from '@angular/material/divider';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-panel',
-  providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatTab, MatTabGroup, MatDivider],
+  providers: [provideNativeDateAdapter(), 
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+  ],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatTab, MatTabGroup, MatDivider, MatExpansionModule],
   templateUrl: './panel.html',
   styleUrl: './panel.css',
 })
