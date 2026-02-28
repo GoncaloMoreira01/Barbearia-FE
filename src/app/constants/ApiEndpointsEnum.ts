64daecb352df3c4c0777aa70ddd74125c894c96b
GoncaloMoreira01 <goncalomoreira01@gmail.com>
2026-02-28 14:15:53 +0000

    [TASK] Added feature: give a testimonial

--- src/app/constants/ApiEndpointsEnum.ts
+++ src/app/constants/ApiEndpointsEnum.ts
@@ -1,3 +1,5 @@
 export const ApiEndpoints = {
   USER_LOGIN: 'http://localhost:8080/barbershop/users/login',
+  
+  CREATE_TESTIMONIAL: 'http://localhost:8080/barbershop/testimonials/create',
 };
--- src/app/panel/panel.ts
+++ src/app/panel/panel.ts
@@ -8,17 +8,56 @@ import { MatTabGroup } from '@angular/material/tabs';
 import { MatDivider } from '@angular/material/divider';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
+import { Authentication } from '../services/authentication';
+import { HttpClient } from '@angular/common/http';
+import { ApiEndpoints } from '../constants/ApiEndpointsEnum';
 
+export interface Testimonial {
+  userId: number;
+  stars: number;
+  review: string;
+}
 
 @Component({
   selector: 'app-panel',
   providers: [provideNativeDateAdapter(), 
     { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
   ],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatTab, MatTabGroup, MatDivider, MatExpansionModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatTab, MatTabGroup, MatDivider, MatExpansionModule, FormsModule, Authentication],
   templateUrl: './panel.html',
   styleUrl: './panel.css',
 })
 export class Panel {
+  constructor(private http: HttpClient, public auth: Authentication) {}
 
+  rating: number = 0;
+  reviewText: string = '';
+
+  submitReview() {
+    const testimonial = {
+    userId: this.auth.getUserLogged()?.id,
+    stars: this.rating,
+    review: this.reviewText
+    };
+
+    const errorMsg = document.getElementById("errorMsg") as HTMLElement;
+    errorMsg.style.display = "none";
+
+    if (this.reviewText == '') {
+      errorMsg.style.display = "initial";
+      return;
+    }
+
+    this.http.post<string>(ApiEndpoints.CREATE_TESTIMONIAL, { testimonial })
+      .subscribe({
+        next: response => {
+          console.log('testimonial created')
+          
+        },
+        error: err => {
+        console.error('testimonial creation failed', err);
+        }
+    });
+  }
 }
